c4de94843beefcdf788c5cd7dc95159069a26303
Guilherme Henrique <guibork@hotmail.com>
2018-04-10 13:45:34 -0300

    Refatorado

--- src/Tp1IotMainJFrame.java
+++ src/Tp1IotMainJFrame.java
@@ -1,4 +1,4 @@
-package tp1iot.src;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
--- src/Tp1IotReader.java
+++ src/Tp1IotReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp1iot.src;
+
 
 import com.alien.enterpriseRFID.reader.*;
 import com.alien.enterpriseRFID.tags.*;
